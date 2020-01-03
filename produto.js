0d7e198a5ab55cd64e0fb5e2cbdbb1216e48cf40
mateusisrael <mateuss.i.fernandes@gmail.com>
2020-01-03 04:25:06 -0300

    .

--- produto.js
+++ produto.js
@@ -15,18 +15,21 @@ function getProduto() {
 function addProdutoVizualizados(nome, uri, img) {
     let vizualizados_recentemente = JSON.parse(sessionStorage.getItem("vizualizados_recentemente"));
     let produto = {nome: nome, uri: uri, img: img};
-    console.log(vizualizados_recentemente.length);
 
+    console.log('3 vizualizados:', vizualizados_recentemente.length);
 
+    
     if(vizualizados_recentemente.length !== 0) {
-        
-        console.log('produto.nome:', typeof produto.nome);
-        for(let i in vizualizados_recentemente) {
-            console.log(`Rodando o for: ${i}`);
-            console.log('vizualizados_recentemente: ',typeof vizualizados_recentemente[i].nome);
-            if(vizualizados_recentemente[i].nome === produto.nome){
+
+        for(let i = 0; i < 11; i++) {
+            console.log(i);
+
+            console.log(vizualizados_recentemente[i].nome);
+            
+            if(vizualizados_recentemente[i].nome === produto.nome) {
                 console.log("Esse produto já está na lista");
-            } else {
+
+            } else if(vizualizados_recentemente[i].nome !== produto.nome) {
                 vizualizados_recentemente.push(produto);
                 vizualizados_recentemente = JSON.stringify(vizualizados_recentemente);
                 sessionStorage.setItem("vizualizados_recentemente", vizualizados_recentemente);
@@ -34,11 +37,18 @@ function addProdutoVizualizados(nome, uri, img) {
                 break;
             }
         }
+
+
+            
+
+
+            
+        
     } else {
         vizualizados_recentemente.push(produto);
         vizualizados_recentemente = JSON.stringify(vizualizados_recentemente);
         sessionStorage.setItem("vizualizados_recentemente", vizualizados_recentemente);
-        console.log("Novo produto adicionado");
+        console.log("Novo produto adicionado 2");
     }
 }
 
