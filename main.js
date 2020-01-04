5d101af62e37c2783e28f001fc4e763fefd9d381
mateusisrael <mateuss.i.fernandes@gmail.com>
2020-01-04 14:02:56 -0300

    .

--- main.js
+++ main.js
@@ -15,20 +15,5 @@ ativarSessionStorage();
 
 
 function vizualizadosRecentemente() {
-    if(sessionStorage.getItem("vizualizados_recentemente") !== "null") {
-        let vizualizados_recentemente = JSON.parse(sessionStorage.getItem("vizualizados_recentemente"));
-        let cVizualizados = document.querySelector("#cVizualizados");
 
-
-        for(let i in vizualizados_recentemente) {
-
-            cVizualizados.innerHTML = cVizualizados.innerHTML +
-            `<a href="${vizualizados_recentemente[i].uri}">
-            <div class="produto">
-               <img src="${vizualizados_recentemente[i].img}" alt="" class="produto-img">
-               <h2 class="produto-name">${vizualizados_recentemente[i].nome}</h2>
-            </div>
-            </a>`;
-        }
-    }
 }
--- produto.js
+++ produto.js
@@ -5,6 +5,7 @@ function ativarSessionStorage() {
         getProduto();
     } else {
         sessionStorage.setItem("vizualizados_recentemente", "[]");
+        getProduto();
         console.log("vizualizados_recentement ativo agora.");
     }
 }
@@ -12,15 +13,15 @@ function ativarSessionStorage() {
 
 function getProduto() {
     let produto_uri =  window.location.href;
-    let produto_img_uri = document.querySelector(".produto-img").src;
-    let produto_nome = document.querySelector(".produto-name").innerHTML;
+    let produto_img_uri = document.querySelector(".cloudzoom").src;
+    let produto_nome = document.querySelector(".name").innerHTML;
     addProdutoVizualizados(produto_nome, produto_uri, produto_img_uri);
 }
 
 
 function addProdutoVizualizados(nome, uri, img) {
     let vizualizados_recentemente = JSON.parse(sessionStorage.getItem("vizualizados_recentemente"));
-    let produto = {nome: nome, uri: uri, img: img};
+    let produto = { nome: nome, uri: uri, img: img };
     let produto_na_lista = false;
     if(vizualizados_recentemente.length !== 0) {
 
@@ -54,5 +55,31 @@ function addProdutoVizualizados(nome, uri, img) {
         vizualizados_recentemente = JSON.stringify(vizualizados_recentemente);
         sessionStorage.setItem("vizualizados_recentemente", vizualizados_recentemente);
         console.log("Novo produto adicionado 2");
+    }
+}
+
+if(sessionStorage.getItem("vizualizados_recentemente") !== "null") {
+    let vizualizados_recentemente = JSON.parse(sessionStorage.getItem("vizualizados_recentemente"));
+    let bgGeral = document.querySelector(".bgGeral");
+    let cVizualizados = document.createElement("div");
+        cVizualizados.setAttribute("id", "cVizualizados");
+        cVizualizados.setAttribute("class", "cVizualizados");
+        /*
+        vizualizados_recentemente.length <= 5 ? cVizualizados.setAttribute("class", "cVizualizados") :
+                                                cVizualizados.setAttribute("class", "caroselcVizualizados");
+        */
+
+    bgGeral.appendChild(cVizualizados);
+
+
+    for(let i in vizualizados_recentemente) {
+
+        cVizualizados.innerHTML = cVizualizados.innerHTML +
+        `<a href="${vizualizados_recentemente[i].uri}">
+        <div class="produto-vizualizado">
+           <img src="${vizualizados_recentemente[i].img}" alt="" class="produto-vizualizado-img">
+           <p class="produto-vizualizado-nome">${vizualizados_recentemente[i].nome}</p>
+        </div>
+        </a>`;
     }
 }
